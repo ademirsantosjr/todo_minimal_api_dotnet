669e052137b8cc3ffb8084b1a463760c3d1f5b0c
Ademir Santos <ademir.m.junior@gmail.com>
2024-12-21 11:16:10 -0300

    Cria camada inicial de autenticacao.

--- TodoMinimalApi/Program.cs
+++ TodoMinimalApi/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using TodoMinimalApi.Data;
@@ -36,6 +37,34 @@ builder.Services.AddAuthorization();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+    {
+        Description = "Autorização JWT usando o esquema Bearer (Exemplo: 'Bearer 12345abcdef')",
+        Name = "Authorization",
+        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+        Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    options.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
+    {
+        {
+            new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+            {
+                Reference = new Microsoft.OpenApi.Models.OpenApiReference
+                {
+                    Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] { }
+        }
+    });
+});
+
 
 // Add services to the container.
 
@@ -117,6 +146,51 @@ app.MapDelete("/api/v1/todos/{id}", async (int id, TodoDbContext dbContext, Clai
     await dbContext.SaveChangesAsync();
     return Results.NoContent();
 }).RequireAuthorization();
+
+app.MapPost("/api/v1/auth/login", (UserLogin userLogin, TodoDbContext dbContext) =>
+{
+    var user = dbContext.Users.FirstOrDefault(u => u.Email == userLogin.Email && u.PasswordHash == userLogin.Password);
+    if (user == null)
+    {
+        return Results.Unauthorized();
+    }
+
+    var claims = new[]
+    {
+        new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+        new Claim(ClaimTypes.Email, user.Email)
+    };
+
+    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("@TodoMinimalApi#1d&mv&lle1d&mn0lle"));
+    var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+    var token = new JwtSecurityToken(
+        issuer: "TodoMinimalApi",
+        audience: "TodoMinimalApi",
+        claims: claims,
+        expires: DateTime.UtcNow.AddHours(1),
+        signingCredentials: credentials
+    );
+
+    var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+    return Results.Ok(new { Token = tokenString });
+});
+
+app.MapGet("/seed", (TodoDbContext dbContext) =>
+{
+    var passwordHash = "senha123";
+    if (!dbContext.Users.Any())
+    {
+        dbContext.Users.Add(new User
+        {
+            Name = "Usuario",
+            Email = "user@example.com",
+            PasswordHash = passwordHash
+        });
+        dbContext.SaveChanges();
+    }
+    return Results.Ok("User seeded.");
+});
 
 app.Run();
 
